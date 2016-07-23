dae067930b55f5eca79da16a10e0bfd9394bd4e1
Tuan.M.Le <Tuan.M.Le@sherpasystems.com.au>
2016-07-23 09:17:00 +0700

    test

--- __tests__/subtract-test.js
+++ __tests__/subtract-test.js
@@ -2,7 +2,7 @@ jest.unmock('../subtract/subtract'); // unmock to use the actual implementation 
 
 describe('subtract', function () {
     it('subtract 2 - 1 to equal 1', function () {
-        const subtract = require('../subtract/subtract');
+        var subtract = require('../subtract/subtract');
         expect(subtract(2, 1)).toBe(1);
     });
 });
--- __tests__/sum-test.js
+++ __tests__/sum-test.js
@@ -2,7 +2,7 @@ jest.unmock('../sum/sum'); // unmock to use the actual implementation of sum
 
 describe('sum', function () {
     it('sum 2 + 1 to equal 3', function () {
-        const sum = require('../sum/sum');
+        var sum = require('../sum/sum');
         expect(sum(2, 1)).toBe(3);
     });
 });
